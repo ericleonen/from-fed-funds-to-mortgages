31353d5fe41645fc8367ecc299053aa360025c56
ericleonen <ericleonen@gmail.com>
2025-08-16 13:40:14 -0700

    add middleware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from helpers import get_plotting_data
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://ericleonen.github.io/from-fed-funds-to-mortgages"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 @app.get("/plotting_data")
 def plot_data():
